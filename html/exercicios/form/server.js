2185c0ff87d9f1e732d68e00b697e9dba487b3f0
Diogo Albuquerque <albuquerquediogo@hotmail.com>
2021-11-29 21:36:56 -0300

    Finalizando conteudo sobre tags HTML

--- html/exercicios/form/server.js
+++ html/exercicios/form/server.js
@@ -6,13 +6,14 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.post('/usuarios', (req,resp) => {
     console.log(req.body)
+   
     resp.send("Parabéns, INCLUIDO")
 })
 
 app.post('/usuarios/:id', (req,resp) => {
     console.log(req.params.id)
     console.log(req.body)
-    resp.send("Parabéns, ALTERADO")
+    resp.send("Parabéns, ALTERADO hahaha")
 })
 
 app.listen(3003)
