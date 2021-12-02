ba6f55ec85a8f02edbe171c91fedc7079c0e0fef
Diogo Albuquerque <albuquerquediogo@hotmail.com>
2021-12-02 17:05:17 -0300

    Desenvolvimento da seção sobre Funções

---
+++ js/funcao/closure.js
@@ -0,0 +1,19 @@
+
+
+// Closure
+
+// Contexto Léxico em ação!
+
+const x = 'Global'
+
+function fora() {
+    const x = 'Local'
+    function dentro() {
+        return x
+    }
+    return dentro
+}
+
+const minhaFuncao = fora()
+
+console.log(minhaFuncao())
